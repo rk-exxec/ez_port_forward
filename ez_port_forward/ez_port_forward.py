6dc1505eaa0962e9150527ae3d2214bcafa9574c
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2024-03-16 19:12:52 +0100

    better error handling and logging

--- ez_port_forward/ez_port_forward.py
+++ ez_port_forward/ez_port_forward.py
@@ -38,7 +38,8 @@ def parse_protocols(prot_obj):
     # returns dict that maps source ports to dest ports
     if not prot_obj: return None
     # bool is an invalid datatype, but would be accepted by the int check later
-    if isinstance(prot_obj, bool): return None
+    if isinstance(prot_obj, bool): 
+        return None
 
     # if ports are given as comma separated list, it is read as text.
     # if only list is given, we assume identical source and dest ports
@@ -49,9 +50,25 @@ def parse_protocols(prot_obj):
         return {prot_obj:prot_obj}
     # yaml is read as dict, we only convert keys and values to ints
     elif isinstance(prot_obj, dict):
-        return {int(k):int(v) for k,v in prot_obj.items()}
+        out_dict = dict()
+        for k,v in prot_obj.items():
+            try:
+                if v is None:
+                    v = k
+                elif isinstance(v, bool):
+                    if v: # if true then assume identical in and out ports
+                        v = k
+                    else: # if false then invalid
+                        raise ValueError()
+                if isinstance(v, float) or isinstance(k, float):
+                    raise ValueError()
+                out_dict[int(k)] = int(v)
+            except ValueError:
+                logger.error(f"Some port information is invalid: ({k}:{v}  <{type(k).__name__}>:<{type(v).__name__}>). Please check source!")
+        return out_dict if out_dict else None
     # if yaml is incorrectly formatted, or entry is missing
     else: 
+        logger.error(f"Invalid datatype for ports: {type(prot_obj)}")
         return None
 
 
@@ -63,13 +80,18 @@ def build_command(protocol:str, bridge:str, target_ip:ipaddress.IPv4Address, tar
 def write_container_commands(file: TextIOWrapper, ip:ipaddress.IPv4Address, bridge:str, tcp_rules=None, udp_rules=None, tcpudp_rules=None, ssh=None):
 
     def write_rules_helper(prot, src, dest):
+        logger.debug(f"\tP:{prot}, S:{src}, D:{dest}")
+        # if something else is wrong and the port is invalid
+        if(not (src and dest)):
+            logger.warning(f"Either Port {src} or {dest} is missing or invalid.")
+            return
         # if port number too large
         if(src > 65535 or dest > 65535):
-            logging.warning(f"Port {src} or {dest} larger than max allowed (65535). Port forward will be commented out in the file.")
+            logger.warning(f"Port {src} or {dest} larger than max allowed (65535). Port forward will be commented out in the file.")
             file.write("#") # if the command is declared invalid it is still written to file, just commented out
         # if port was already opened
         if (src,prot) in existing_port_maps.keys():
-            logging.warning(f"Port {src} already opened for {existing_port_maps[(src,prot)]}. Port forward will be commented out in the file.")
+            logger.warning(f"Port {src} already opened for {existing_port_maps[(src,prot)]}. Port forward will be commented out in the file.")
             file.write("#") # if the command is declared invalid it is still written to file, just commented out
         else:
             existing_port_maps[(src,prot)] = ip
@@ -97,7 +119,7 @@ def parse_yaml(yaml_file):
         try:
             yaml_dict : dict = yaml.load(conf, Loader)
         except yaml.YAMLError as ye:
-            logging.error(f"Error while parsing YAML file:  {str(ye)}")
+            logger.error(f"Error while parsing YAML file:  {str(ye)}")
             raise
 
     return yaml_dict
@@ -106,12 +128,14 @@ def write_iptables_file(yaml_dict, out_file):
     with open(out_file, "w") as output:
 
         for iname,iconf in yaml_dict.items():
+            output.write("#===========================\n")
+            logger.debug(f"Entering interface {iname}")
             output.write(f"iface {iname} inet static\n")
             bridge = iconf.pop("bridge")
             subnet = ipaddress.IPv4Network(iconf.pop("subnet"))
-
             for cont_id, cont_conf in iconf["forwards"].items():
                 output.write(f"#--- Container {cont_id}\n")
+                logger.debug(f"Parsing rules for container {cont_id}")
                 try:
                     # this automatically creates an ip offset by the container id from base address in subnet and checks validity
                     container_ip = subnet[cont_id]
@@ -119,40 +143,53 @@ def write_iptables_file(yaml_dict, out_file):
                     # check if ssh key in dict, and if it has a specified value
                     if "ssh" in cont_conf.keys():
                         ssh = cont_conf["ssh"]
-                        if isinstance(ssh,bool) or not isinstance(ssh,int): 
+                        if isinstance(ssh,str):
+                            logger.error(f"Container {cont_id}: SSH only supports single number values.")
+                            ssh = None
+                        elif (ssh and isinstance(ssh,bool)) or ssh is None: 
                             ssh = {cont_id*100 + 22:22} # assign default if key exists and no int value is specified
+                        elif isinstance(ssh, int):
+                            if ssh >= 100:
+                                logging.error(f"Container {cont_id}: SSH port above 100 not supported. Please use TCP instead.")
+                                ssh = None
+                            else: ssh = {cont_id*100 + ssh:ssh} # use specified internal ssh port if given
                         else:
-                            ssh = {cont_id*100 + ssh:ssh} # use specified internal ssh port if given
+                            logger.error(f"Container {cont_id}: Invalid SSH value type: {ssh} ({type(ssh)})")
+                            ssh = None
 
                     else: ssh = None
-                            
+
                     tcp = parse_protocols(cont_conf.pop("tcp", None))
                     udp = parse_protocols(cont_conf.pop("udp", None))
                     tcpudp = parse_protocols(cont_conf.pop("tcpudp", None))
+
+                    logger.debug(f"Writing rules for container {cont_id}")
+                            
                     write_container_commands(output, container_ip, bridge, tcp, udp, tcpudp, ssh)
                 except Exception as ex:
-                    logging.error(f"Error while parsing rules for Container ID {cont_id} for interface <{iname}> with subnet <{subnet}>: \n{ex}")
+                    logger.error(f"Error while parsing rules for Container ID {cont_id} for interface <{iname}> with subnet <{subnet}>: \n{ex}")
                     output.write("#  ERROR -- See console log.")
                     continue
-                finally:       
-                    output.write("#---\n")
+
+            output.write("#===========================\n")
 
 
-def main():
+def main(args=None):
     logger.setLevel(logging.INFO)
-
-
+    ch = logging.StreamHandler(sys.stdout)
+    logger.addHandler(ch)
     parser = argparse.ArgumentParser()
     parser.add_argument("input_yaml", type=str, default="./port_conf.yaml", help="The input yaml file. Defaults to ./port_conf.yaml", nargs="?")
     parser.add_argument("-o", "--output", type=str, default="/etc/network/interfaces.d/port_forwards", help="Target path. Optional, defaults to /etc/network/interfaces.d/port_forwards", nargs="?")
-    
-    args = parser.parse_args()
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args(args)
 
     yaml_path = Path(args.input_yaml)
     out_path = Path(args.output)
-    
-    assert yaml_path.exists(), "Input file does not exist."
-    # assert out_path, "Output path does not exist or is not writeable."
+    if args.verbose: logger.setLevel(logging.DEBUG)
+
+    assert yaml_path.exists(), f"Input file does not exist. {yaml_path}"
+
 
     yaml_dict = parse_yaml(yaml_path)
 
